0d50985438e99a7e68e48646a8422e78114bc9af
zakiverox <zakiverox@gmail.com>
2018-06-29 20:39:18 +0700

    Add Colletion Class

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,13 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+          'name' => $this->name,
+          'price' => $this->hjual,
+            'reting' => $this->review->count() > 0 ? round($this->review->sum('star')/$this->review->count(),2) : 'No reting yet',
+            'herf'=>[
+              'link' => route('products.show',$this->id)
+            ]
+        ];
     }
 }
