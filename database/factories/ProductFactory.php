9f629db467f47234927b91da5792f0f3e2b3c75d
zakiverox <zakiverox@gmail.com>
2018-06-29 17:17:59 +0700

    Migration Seeding for product and Review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+      'name' =>$faker->word,
+      'detail'=>$faker->paragraph,
+      'hjual'=>$faker->numberBetween(100,1000),
+      'stok'=>$faker->randomDigit
     ];
 });
