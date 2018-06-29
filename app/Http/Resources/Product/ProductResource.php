faf0bb01c62f4d8734a3bd0d9f492f22fc232234
zakiverox <zakiverox@gmail.com>
2018-06-29 19:01:48 +0700

    Hnadle Respond On Data

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+          'name' => $this->name,
+          'description' => $this->detail,
+          'price' =>$this->hjual,
+          'stok' => $this->stok
+        ];
+    }
+}
